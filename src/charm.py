04f16f962dddbc37331b0fd54494a95660eb4685
Dominik Fleischmann <dominik.fleischmann@canonical.com>
2021-11-22 12:33:18 +0100

    Fix Status Handling and License Headers
    - Fix Status Handling by always calling main and adding checks
    - Add license headers and checks
    - Fix charmcraft parts issue

--- src/charm.py
+++ src/charm.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
 
 import logging
 import yaml
@@ -22,52 +24,35 @@ class Operator(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        if not self.unit.is_leader():
-            # We can't do anything useful when not the leader, so do nothing.
-            self.model.unit.status = WaitingStatus("Waiting for leadership")
-            return
+
         self.log = logging.getLogger(__name__)
+
         self.profile_image = OCIImageResource(self, "profile-image")
         self.kfam_image = OCIImageResource(self, "kfam-image")
-
-        try:
-            self.interfaces = get_interfaces(self)
-        except NoVersionsListed as err:
-            self.model.unit.status = WaitingStatus(str(err))
-            return
-        except NoCompatibleVersions as err:
-            self.model.unit.status = BlockedStatus(str(err))
-            return
 
         for event in [
             self.on.install,
             self.on.leader_elected,
             self.on.upgrade_charm,
             self.on.config_changed,
+            self.on["kubeflow-profiles"].relation_changed,
         ]:
             self.framework.observe(event, self.main)
 
-        self.framework.observe(
-            self.on["kubeflow-profiles"].relation_changed, self.send_info
-        )
-
-    def send_info(self, event):
-        if self.interfaces["kubeflow-profiles"]:
-            self.interfaces["kubeflow-profiles"].send_data(
-                {
-                    "service-name": self.model.app.name,
-                    "service-port": str(self.model.config["port"]),
-                }
-            )
-
     def main(self, event):
         try:
+            self._check_leader()
+
+            interfaces = self._get_interfaces()
+
             profile_image_details = self.profile_image.fetch()
             kfam_image_details = self.kfam_image.fetch()
-        except OCIImageResourceError as e:
-            self.model.unit.status = e.status
-            self.log.info(e)
+
+        except (CheckFailed, OCIImageResourceError) as error:
+            self.model.unit.status = error.status
             return
+
+        self._send_info(event, interfaces)
 
         namespace_labels_filename = "namespace-labels.yaml"
 
@@ -183,6 +168,40 @@ class Operator(CharmBase):
         self.log.info("pod.set_spec() completed without errors")
 
         self.model.unit.status = ActiveStatus()
+
+    def _send_info(self, event, interfaces):
+        if interfaces["kubeflow-profiles"]:
+            interfaces["kubeflow-profiles"].send_data(
+                {
+                    "service-name": self.model.app.name,
+                    "service-port": str(self.model.config["port"]),
+                }
+            )
+
+    def _check_leader(self):
+        if not self.unit.is_leader():
+            # We can't do anything useful when not the leader, so do nothing.
+            raise CheckFailed("Waiting for leadership", WaitingStatus)
+
+    def _get_interfaces(self):
+        try:
+            interfaces = get_interfaces(self)
+        except NoVersionsListed as err:
+            raise CheckFailed(err, WaitingStatus)
+        except NoCompatibleVersions as err:
+            raise CheckFailed(err, BlockedStatus)
+        return interfaces
+
+
+class CheckFailed(Exception):
+    """ Raise this exception if one of the checks in main fails. """
+
+    def __init__(self, msg, status_type=None):
+        super().__init__()
+
+        self.msg = msg
+        self.status_type = status_type
+        self.status = status_type(msg)
 
 
 if __name__ == "__main__":
