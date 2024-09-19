191e00d2610e3ca9e58484cd8c1b53910c9dd321
Manos Vlassis <57320708+mvlassis@users.noreply.github.com>
2024-09-19 14:24:15 +0300

    Feat: Add Terraform module (#190)
    * Add Terraform module
    
    * Update format
    
    * Change version compatibility

---
+++ terraform/outputs.tf
@@ -0,0 +1,16 @@
+output "app_name" {
+  value = juju_application.kubeflow_profiles.name
+}
+
+output "provides" {
+  value = {
+    kubeflow_profiles = "kubeflow-profiles"
+    metrics_endpoint  = "metrics-endpoint"
+  }
+}
+
+output "requires" {
+  value = {
+    logging = "logging"
+  }
+}
