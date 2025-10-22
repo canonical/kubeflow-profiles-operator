b8a42f229642b4cbbd9f3a678e339ca4f968b897
mvlassis <manos.vlassis@canonical.com>
2025-10-22 14:05:59 +0200

    Pinning base to 24.04

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 resource "juju_application" "kubeflow_profiles" {
   charm {
     name     = "kubeflow-profiles"
+    base     = var.base
     channel  = var.channel
     revision = var.revision
   }
