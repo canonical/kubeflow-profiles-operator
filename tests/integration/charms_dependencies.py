06cfac14e0b597f161688906b5cf6d872eb7ad27
Dario Faccin <dario.faccin@canonical.com>
2026-05-22 09:32:01 +0200

    chore: pin charm dependencies in tests (track/1.10)

--- tests/integration/charms_dependencies.py
+++ tests/integration/charms_dependencies.py
@@ -2,4 +2,4 @@
 
 from charmed_kubeflow_chisme.testing import CharmSpec
 
-ISTIO_PILOT = CharmSpec(charm="istio-pilot", channel="1.24/stable", trust=True)
+ISTIO_PILOT = CharmSpec(charm="istio-pilot", channel="1.28/stable", trust=True)
